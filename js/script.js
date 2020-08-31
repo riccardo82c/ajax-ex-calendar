0b7869c6adeea13c30711cbf8fbdb0a6a55b40bf
Riccardo Colombo <rikyregna@gmail.com>
2020-08-31 19:36:20 +0200

    second milestone DONE (style to do)

--- js/script.js
+++ js/script.js
@@ -32,63 +32,74 @@ $(function () {
 
 	// carico il template dell 'oggetto in una variabile 
 	var html = template(meseCalendario);
+
 	// la appendo nel DOM in container
 	$('.container').append(html);
 
-	// al click del bottone aggiungo o tolgo un mese
+	// al click del bottone aggiungo o tolgo un mese e ristampo i giorni
 	$('#prev').click(function () {
-		initDate = initDate.add('-1', 'M');
-		console.log(initDate.format('DD MMMM YYYY'));
-		console.log('prev');
-		meseAnno = initDate.format('MMMM YYYY').toUpperCase();
+
+		if (initDate.format('MM') !== '01') {
+			changeMonth('-1');
+			printDay();
+
+		} else {
+			alert('errore');
+		}
+
+
 
 	})
 
 	$('#next').click(function () {
-		initDate = initDate.add('1', 'M');
-		console.log(initDate.format('DD MMMM YYYY'));
-		console.log('next');
-		meseAnno = initDate.format('MMMM YYYY').toUpperCase();
+		if (initDate.format('MM') !== '12') {
+			changeMonth('1');
+			printDay();
+
+		} else {
+			alert('errore');
+		}
 	})
 
 
-	// salvo in una var i giorni del mese corrente
-	let dayInMonth = moment(meseCalendario.meseAnno, 'MMMM YYYY').daysInMonth();
+	printDay();
+	setHolidays(initDate.format('M') - 1);
 
 
-	// creo un ciclo per stampare i giorni correnti
-	for (let i = 1; i <= dayInMonth; i++) {
-		$('.days').append(`<p data-set='${initDate.format('YYYY-MM')}-${addZero(i)}'>${addZero(i)} ${initDate.format('MMMM')} </p>`);
-	}
 
 
-	// chiamata AJAX
+	function setHolidays(int) {
 
-	$.ajax({
-		method: 'GET',
-		url: 'https://flynn.boolean.careers/exercises/api/holidays',
-		data: {
-			year: 2018,
-			month: 0
-		},
-		success: function (data) {
+		// chiamata AJAX
+		$.ajax({
+			method: 'GET',
+			url: 'https://flynn.boolean.careers/exercises/api/holidays',
+			data: {
+				/* year: initDate.format('YYYY'),
+				month: initDate.format('M') */
+				year: 2018,
+				month: int
+			},
+			success: function (obj) {
 
-			for (let i = 0; i < data.response.length; i++) {
-				// console.log(data.response[i].date);
-				let currentday = $(`p[data-set='${data.response[i].date}'`)
-				currentday.addClass('holiday').append(data.response[i].name);
+				for (let i = 0; i < obj.response.length; i++) {
+					// console.log(data.response[i].date);
+					let currentday = $(`p[data-set='${obj.response[i].date}'`)
+					currentday.addClass('holiday').append(obj.response[i].name);
+				}
+
+				// $('p').each(function (index, value) {
+				// 	let a = $(this).text();
+				// 	console.log(a);
+				// });
+
+			},
+			error: function () {
+
 			}
+		});
 
-			// $('p').each(function (index, value) {
-			// 	let a = $(this).text();
-			// 	console.log(a);
-			// });
-
-		},
-		error: function () {
-
-		}
-	});
+	};
 
 
 	/* funzioni */
@@ -98,6 +109,42 @@ $(function () {
 		if (int < 10) {
 			return (`0${int}`);
 		} else return int;
+	}
+
+	// cambio mese
+
+	function changeMonth(int) {
+		initDate = initDate.add(int, 'M');
+
+		meseAnno = initDate.format('MMMM YYYY').toUpperCase();
+
+		$('.container').empty();
+		var source = $("#entry-template").html();
+		var template = Handlebars.compile(source);
+		let meseCalendario = {
+			meseAnno: initDate.format('MMMM YYYY').toUpperCase(),
+		};
+		var html = template(meseCalendario);
+		$('.container').append(html);
+		setHolidays(initDate.format('M') - 1);
+
+
+	}
+
+
+	// stampa i giorni
+
+	function printDay() {
+
+		$('.days').empty();
+		// salvo in una var i giorni del mese corrente
+		let dayInMonth = moment(initDate, 'MMMM YYYY').daysInMonth();
+		// creo un ciclo per stampare i giorni correnti
+		for (let i = 1; i <= dayInMonth; i++) {
+			$('.days').append(`<p data-set='${initDate.format('YYYY-MM')}-${addZero(i)}'>${addZero(i)} ${initDate.format('MMMM')} </p>`);
+		}
+
+
 	}
 
 
